af1ff7ef807dec9c60824cd79eef333a92506da8
Lucas Scarlato Astur <lucas.astur@ibm.com@air-de-lucas.br.ibm.com>
2019-06-25 14:33:05 -0300

    fix no spaces bug

--- Teste1/MovieSorter/src/main/java/com/moviesorter/MovieSorter.java
+++ Teste1/MovieSorter/src/main/java/com/moviesorter/MovieSorter.java
@@ -1,51 +1,55 @@
 package com.moviesorter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class MovieSorter {
 static String URL_STRING = "https://jsonmock.hackerrank.com/api/movies/search/?Title=";
 	
-	static JSONObject getJSONResponse(String params) throws IOException {
+	static JsonObject getJSONResponse(String params) throws IOException {
 		URL url = new URL(URL_STRING + params);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
 		
-		Scanner scanner = new Scanner(url.openStream());
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 		
 		String response = "";
-		while(scanner.hasNext()) {
-			response += scanner.next();
+		String line = "";
+		while((line = br.readLine()) != null) {
+			response += line;
 			
 		}
-		scanner.close();
+		br.close();
 		
-		return new JSONObject(response);
+		return new Gson().fromJson(response, JsonObject.class);
 	}
 	
 	static int getTotalPages(String movie_substr) throws IOException {
-		JSONObject json_parser = getJSONResponse(movie_substr);
-		return json_parser.getInt("total_pages");
+		JsonObject json_parser = getJSONResponse(movie_substr);
+		return json_parser.get("total_pages").getAsInt();
 	}
 	
 	static int getTotalMovies(String movie_substr) throws IOException {
-		JSONObject json_parser = getJSONResponse(movie_substr);
-		return json_parser.getInt("total");
+		JsonObject json_parser = getJSONResponse(movie_substr);
+		return json_parser.get("total").getAsInt();
 	}
 	
 	static String[] getMoviesOnPage(String movie_substr, int page) throws IOException{
-		JSONObject json_parser = getJSONResponse(movie_substr + "&page=" + Integer.toString(page));
-		JSONArray movie_array = json_parser.getJSONArray("data");
-		String movie_names[] = new String[movie_array.length()];
+		JsonObject json_parser = getJSONResponse(movie_substr + "&page=" + Integer.toString(page));
+		JsonArray movie_array = json_parser.get("data").getAsJsonArray();
+		String movie_names[] = new String[movie_array.size()];
 		
 		for(int i = 0; i< movie_names.length; i++) {
-			movie_names[i] = movie_array.getJSONObject(i).getString("Title");
+			movie_names[i] = movie_array.get(i).getAsJsonObject().get("Title").getAsString();
 		}
 		
 		return movie_names;
